af5989be0a9bfd6f26f8ce6c958214fc57e5b1cc
Sam Stewart <samstew367@gmail.com>
2026-05-05 16:51:22 +0100

    implement target spawning

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //HTML ELEMENTS
+const game_board = document.getElementById("game-board")
 const top_bar = document.getElementById("top-bar")
 const top_bar_content = document.getElementById("top-bar-content")
 const difficulty_section = document.getElementById("difficulty-section")
@@ -11,10 +12,16 @@ const hard_button = document.getElementById("hard-button")
 const start_button_container = document.getElementById("start-button-container")
 const start_button = document.getElementById("start-button")
 
+//target image
+
+
 
 //DIFFICULTY VARIABLE
 //easy by default
 let difficulty = 1
+
+//Score variable
+let score = 0
 
 
 
@@ -84,6 +91,51 @@ hard_button.addEventListener ("click", function(){
 //START THE GAME
 start_button.addEventListener ("click", function(){
 
-    game_loop()
+    //set all menu elements to display none
 
+    top_bar.innerText = "none"
+    difficulty_section.style.display = "none"
+    start_button.style.display = "none"
+
+    spawn_target()
+   
 })
+
+function spawn_target() {
+
+    //this function creates a button that can be clicked
+
+    //remove old target
+
+    let old_target = document.getElementById("target")
+    if (old_target){
+        old_target.remove()
+    }
+    
+    //add the new target
+    let target = document.createElement("img")
+    target.src = "target.png"
+    target.style.width = "50px"
+    target.style.height = "50px"
+    target.style.position = "absolute"
+    target.id = "target"
+
+    //position randomly
+    target.style.left = Math.random() * 80 + '%'
+    target.style.top = Math.random() * 80 + '%'
+
+    //add the event listener
+    target.addEventListener ("click", function(){
+        score++
+        spawn_target();
+    })
+
+    game_board.appendChild(target)
+    
+    
+
+
+}
+
+
+
